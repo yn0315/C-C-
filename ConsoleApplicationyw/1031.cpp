18c1307289f9ca4e1fef511d44690f06c4a1445f
yn0315 <yunu0315@naver.com>
2022-10-31 13:41:01 +0900

    [2022.10.31]

---
+++ ConsoleApplicationyw/1031.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> //확장자가 h로 끝나는 건 헤더파일임, 입출력 관련된 함수를 쓰려면 stdio.h써야함// 입출력 관련헤더를 사용하겠다 선언하는 것
+// std io 스탠다드 인풋 아웃풋
+
+int main() //main 함수정의
+{
+	//c언어 프로그램에는 반드시 main함수가 있어야 한다.
+	printf("hello,world!\n"); 
+	//hello world 문자열 출력
+
+	
+
+
+}
+
